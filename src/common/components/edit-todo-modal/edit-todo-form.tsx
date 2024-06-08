8ac927160b9bf6892951b13759037936440f698f
Quarticdot AI <makadsuhel11@gmail.com>
2024-06-08 21:31:18 +0530

    refact: Move task schema to new utils

--- src/common/components/edit-todo-modal/edit-todo-form.tsx
+++ src/common/components/edit-todo-modal/edit-todo-form.tsx
@@ -24,6 +24,7 @@ import { Textarea } from "@/components/ui/textarea";
 import { Button } from "@/components/ui/button";
 import { DialogFooter } from "@/components/ui/dialog";
 
+import { taskSchema, type Task } from "@/utils/types";
 import { STATUS, STATUS_OPTIONS } from "@/utils/constant";
 
 export type EditTodoFormProps =
@@ -37,42 +38,19 @@ export type EditTodoFormProps =
       isNew: true;
     };
 
-const editFormSchema = z.object({
-  title: z
-    .string({
-      message: "Title is required",
-    })
-    .trim()
-    .min(1)
-    .max(50),
-  description: z
-    .string({
-      message: "Description is required",
-    })
-    .trim()
-    .min(1)
-    .max(500),
-  status: z.enum(Object.values(STATUS) as [string, ...string[]], {
-    invalid_type_error: "Invalid status type",
-    message: "Status is required",
-  }),
-});
-
 const defaultValues = {
   title: "",
   description: "",
   status: STATUS.IN_PROGRESS,
 };
 
-type EditFormSchema = z.infer<typeof editFormSchema>;
-
 export const EditTodoForm = (props: EditTodoFormProps) => {
-  const form = useForm<EditFormSchema>({
-    resolver: zodResolver(editFormSchema),
+  const form = useForm<Task>({
+    resolver: zodResolver(taskSchema),
     defaultValues: { ...(props.isNew ? defaultValues : props) },
   });
 
-  function onSubmit(values: EditFormSchema) {
+  function onSubmit(values: Task) {
     console.log(values);
   }
 
---
+++ src/common/utils/types.ts
@@ -0,0 +1,25 @@
+import { z } from "zod";
+import { STATUS } from "./constant";
+
+export const taskSchema = z.object({
+  title: z
+    .string({
+      message: "Title is required",
+    })
+    .trim()
+    .min(1)
+    .max(50),
+  description: z
+    .string({
+      message: "Description is required",
+    })
+    .trim()
+    .min(1)
+    .max(500),
+  status: z.enum(Object.values(STATUS) as [string, ...string[]], {
+    invalid_type_error: "Invalid status type",
+    message: "Status is required",
+  }),
+});
+
+export type Task = z.infer<typeof taskSchema>;
