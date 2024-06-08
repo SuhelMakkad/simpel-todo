1bbbcc381e946948b7b04521a6e81f1a0bc527b7
Quarticdot AI <makadsuhel11@gmail.com>
2024-06-08 15:48:00 +0530

    feat: Add user avatar dropdown

--- src/common/components/nav-bar/index.tsx
+++ src/common/components/nav-bar/index.tsx
@@ -1,15 +1,19 @@
-import { ThemeSwitch } from "@/components/theme/theme-switch";
 import Link from "next/link";
+import { ThemeSwitch } from "@/components/theme/theme-switch";
+import { ProfileAvatar } from "@/components/profile-avatar";
 
 export const NavBar = () => {
   return (
     <nav className="shadow-sm border border-b sticky top-0 bg-background">
       <div className="container flex justify-between items-center py-2">
-        <Link href={"/"} className="font-medium">
-          My Tasks
-        </Link>
+        <div className="flex items-center gap-2">
+          <Link href={"/"} className="font-medium">
+            My Tasks
+          </Link>
+          <ThemeSwitch />
+        </div>
 
-        <ThemeSwitch />
+        <ProfileAvatar />
       </div>
     </nav>
   );
---
+++ src/common/components/profile-avatar/index.tsx
@@ -0,0 +1,31 @@
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { LogOut, User } from "lucide-react";
+
+export const ProfileAvatar = () => {
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger>
+        <Avatar>
+          <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
+          <AvatarFallback>CN</AvatarFallback>
+        </Avatar>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent>
+        <DropdownMenuItem>
+          <User className="w-4 h-4 mr-2" />
+          Profile
+        </DropdownMenuItem>
+        <DropdownMenuItem>
+          <LogOut className="w-4 h-4 mr-2" />
+          Logout
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
